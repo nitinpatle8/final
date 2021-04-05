a13ac55f26f1680429ed082fd13f9506b6101fd0
hackerone <hackerone@localhost.localdomain>
2021-04-05 22:31:02 +0000

    my final commit practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router';
+import Home from './Home';
+import About from './About';
+import Service from './Service';
+import Contact from './Contact';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap"
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <div className="container-fluid nav bg">
+                <div className='row'>
+                    <div className='col-10 mx-auto'>
+
+                 
+            <Navbar/>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/about" component={About}/>
+                <Route exact path="/service" component={Service} />
+                <Route exact path="/contact" component={Contact} />
+                <Redirect to="/"/>
+                <Home/>
+            </Switch>
+        </div>
+        </div>
+        </div>
+        </>
+    );
+};
 
 export default App;
